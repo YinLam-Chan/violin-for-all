6a4cb378ebd8819f916358222b7fe9dec3ce7575
Yin Lam Chan (Victoria) <55721038+YinLam-Chan@users.noreply.github.com>
2020-09-14 21:45:25 +1200

    Add tutorials main page

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import '../../App.css';
+import './Login.css';
 
 export default function Login() {
     return (
         <div className='home'>
             <h1 className='login'>Login</h1>
             <form>
-                <label>Email:</label>
+                <label className='label'>Email:</label>
                 <input type="text" name="email"></input>
-
-                <label>Password:</label>
+                <br/>
+                <label className='label'>Password:</label>
                 <input type="text" name="password"></input>
             </form>
 
-            <button type="button">Create New Account</button>
-            <button type="button">Login</button>
+            <button className='create-new' type="button">Create New Account</button>
+            <button className='submit' type="button">Login</button>
         </div>
     )
 }
--- src/components/pages/Tutorials.js
+++ src/components/pages/Tutorials.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import '../../App.css';
+import beginnerviolins from '../images/beginner-violins.jpg';
+import intermediateviolins from '../images/intermediate-violin.jpg';
+import advancedviolins from '../images/advanced-violin.jpg';
+import './Tutorials.css';
 
 export default function Tutorials() {
     return (
         <div className='home'>
             <h1 className='tutorials'>Tutorials</h1>
+
+            <h2 className='level'>Beginner Level</h2>
+
+            <img className='levelImage'src={beginnerviolins} />
+
+            <br />
+            <h2 className='level'>Intermediate Level</h2>
+
+            <img className='levelImage'src={intermediateviolins} />
+
+            <br />
+            <h2 className='level'>Advanced Level</h2>
+
+            <img className='levelImage'src={advancedviolins} />
         </div>
     )
 }
