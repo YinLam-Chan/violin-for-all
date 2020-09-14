11d13fbc1135b4c03ed02ce4c0d0b337d30c4959
Yin Lam Chan (Victoria) <55721038+YinLam-Chan@users.noreply.github.com>
2020-09-15 00:35:56 +1200

    Add tables

--- src/components/pages/SheetMusic.js
+++ src/components/pages/SheetMusic.js
@@ -1,10 +1,88 @@
 import React from 'react';
 import '../../App.css';
+import './SheetMusic.css';
+import * as ReactBootStrap from 'react-bootstrap';
 
 export default function SheetMusic() {
+    const music = [
+        {name: "River flows in you", file: "PDF file download"},
+        {name: "Canon in D", file: "PDF file download"}
+    ]
+
+    const renderMusic = (music, index) => {
+        return (
+            <tr key={index}>
+                <td>{music.name}</td>
+                <td>{music.file}</td>
+            </tr>
+        )
+    }
+
     return (
         <div className='home'>
             <h1 className='sheetMusic'>Sheet Music</h1>
+
+            <h2 className='level'>Easy</h2>
+
+            <div>
+                <ReactBootStrap.Table className='musicTable'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>File</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Mary has a little lamb</td>
+                            <td>PDF file download</td>
+                        </tr>
+                        <tr>
+                            <td>Twinkle twinkle little star</td>
+                            <td>PDF file download</td>
+                        </tr>
+                        <tr>
+                            <td>Happy birthday to you</td>
+                            <td>PDF file download</td>
+                        </tr>
+                    </tbody>
+                </ReactBootStrap.Table >
+
+                <h2 className='level'>Intermediate</h2>
+
+                <ReactBootStrap.Table className='musicTable'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>File</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {music.map(renderMusic)}
+                    </tbody>
+                </ReactBootStrap.Table>
+
+                <h2 className='level'>Difficult</h2>
+
+                <ReactBootStrap.Table className='musicTable'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>File</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Moonlight Sonata</td>
+                            <td>PDF file download</td>
+                        </tr>
+                        <tr>
+                            <td>Swan lake</td>
+                            <td>PDF file download</td>
+                        </tr>
+                    </tbody>
+                </ReactBootStrap.Table>
+            </div>
         </div>
-    )
+    );
 }
