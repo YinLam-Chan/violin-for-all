6a3a67ff4c58a1ed5851e432b0ab0c5480f94d88
Yin Lam Chan (Victoria) <55721038+YinLam-Chan@users.noreply.github.com>
2020-09-16 01:04:48 +1200

    Deleted unused files

--- src/components/pages/Message.js
+++ src/components/pages/Message.js
@@ -11,6 +11,28 @@ export default function Message() {
             <button className='received' type="button">Received message</button>
             <button className='delivered' type="button">Delivered message</button>
             <button className='send' type="button">Send new message</button>
+
+            <ReactBootStrap.Table className='messageTable'>
+                <thead>
+                    <tr>
+                        <th>Message</th>
+                        <th>Sender name</th>
+                        <th>Received date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Do you have the sheet music of Moonlight Sonata? Can you send to me?</td>
+                        <td>Amber Vogue</td>
+                        <td>18 Aug 2020</td>
+                    </tr>
+                    <tr>
+                        <td>Can you give me some tips how to sight read?</td>
+                        <td>Jane Houston</td>
+                        <td>13 Aug 2020</td>
+                    </tr>
+                </tbody>
+            </ReactBootStrap.Table>
         </div>
     )
 }
